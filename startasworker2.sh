338a60c8f19c01a144bbc9d2d4878af1cd4069d2
jiao <jiaolifeng@yahoo.com>
2020-01-09 10:56:35 +0000

    --docker

--- startasworker2.sh
+++ startasworker2.sh
@@ -1,1 +1,1 @@
-sudo ./k3s agent --server https://192.168.2.183 --token K1058227bba2c18866adbe5dea04f4948291dd77fc1deb4d2b91f013ecf37a65b1b::server:3fc58fe3baa4add9553af2c1e800cbd4
+sudo ./k3s agent --docker --server https://192.168.2.183 --token K1058227bba2c18866adbe5dea04f4948291dd77fc1deb4d2b91f013ecf37a65b1b::server:3fc58fe3baa4add9553af2c1e800cbd4
