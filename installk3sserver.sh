5036059843faa0d3bdd8701e6dab474c596538f8
jiao <jiaolifeng@yahoo.com>
2020-01-09 11:02:38 +0000

    new

--- installk3sserver.sh
+++ installk3sserver.sh
@@ -49,6 +49,7 @@ verify_system() {
 	K3S_SERVER_IPPORT=$1
 
 	echo $1 >serveripport.debug
-	echo "sudo ./k3s agent --server https://$K3S_SERVER_IPPORT --token $K3S_TOKEN" > startasworkertemplate.sh
-	cp startasworkertemplate.sh startasworker.sh
+	echo "sudo /root/k3s agent --docker --server https://$K3S_SERVER_IPPORT --token $K3S_TOKEN" > startasworkertemplate.sh
+	cp startasworkertemplate.sh /root/startasworker.sh
+        chmod a+x /root/startasworker.sh
 }
