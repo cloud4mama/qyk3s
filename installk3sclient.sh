a6be031ef30a854428b3efeb87d2381af034859b
jiao <jiaolifeng@yahoo.com>
2020-01-09 11:28:15 +0000

    change to startasworker

--- installk3sclient.sh
+++ installk3sclient.sh
@@ -6,7 +6,7 @@ if [ $# -ne  1 ]; then
 fi
 
 echo "scp  /root/qyk3s2/k3s  root@$1:/root/k3s" > a.sh
-echo "scp  /root/qyk3s2/startasworker2.sh  root@$1:/root/startasworker.sh" >> a.sh
+echo "scp  /root/qyk3s2/startasworker.sh  root@$1:/root/startasworker.sh" >> a.sh
 echo "scp  /root/qyk3s2/qyk3sworker.service root@$1:/etc/systemd/system/qyk3sworker.service" >>a.sh
 echo "ssh  root@$1 systemctl daemon-reload" >>a.sh
 echo "ssh  root@$1 systemctl start qyk3sworker" >>a.sh
