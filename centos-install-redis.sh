40b7e275e66ef3d63da08381fe6ac87ff397ac1a
732047249 <jiaolifeng@yahoo.com>
2020-06-17 21:53:34 +0800

    Update centos-install-redis.sh

--- centos-install-redis.sh
+++ centos-install-redis.sh
@@ -5,4 +5,6 @@ yum install -y http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 yum --enablerepo=remi install redis
  
 service redis start
+
+# https://blog.csdn.net/enmotech/article/details/81230531 stream use case example
  
