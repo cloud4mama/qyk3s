cc0fe45aa5efed96af4eee7b06fb116f42266c0d
732047249 <jiaolifeng@yahoo.com>
2020-06-17 19:36:32 +0800

    Create centos-install-kafka.sh

---
+++ centos-install-kafka.sh
@@ -0,0 +1,8 @@
+# https://www.cnblogs.com/wlsblog/p/7908348.html
+# install java
+yum list java*
+yum list java-1.8*   
+yum install java-1.8.0-openjdk* -y
+java -version
+#install zookeeper https://www.jianshu.com/p/4f7b0a6569d3 zookeeper 
+#install kafka  
